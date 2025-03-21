a57aedd30d7914bcc4df8734c2afa7062f8729ec
tomas pereira <tomaspereira2k@gmail.com>
2025-03-21 16:23:36 -0300

    añadida funcionalidad y tipos al componente lastest articles, configuraciones menores

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,10 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
+      plugins: [tailwindcss()],
+      optimizeDeps: {
+        exclude: ["/home/tomas/blog-astro/node_modules/.vite/deps/chunk-IKH3W5T5.js?v=1eb81e0c"]
+      } 
   },
 
   integrations: [sanity({
